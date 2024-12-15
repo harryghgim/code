bc4c4ac26c798c1dce9880cfebb4c6615b4cfc1e
gwanghyeongim <harryghgim@gmail.com>
2024-12-15 22:38:08 +0900

    add gt and allocate

--- model.py
+++ model.py
@@ -22,26 +22,39 @@ class Batch:
     def allocate(self, line: OrderLine):
         if self.can_allocate(line):
             self._allocations.add(line)
-    
+
     def deallocate(self, line: OrderLine):
         if line in self._allocations:
             self._allocations.remove(line)
-    
+
     @property
     def allocated_quantity(self) -> int:
         return sum(line.qty for line in self._allocations)
-    
+
     @property
     def available_quantity(self) -> int:
         return self._purchased_quantity - self.allocated_quantity
 
     def can_allocate(self, line: OrderLine) -> bool:
         return self.sku == line.sku and self.available_quantity >= line.qty
-    
+
     def __eq__(self, other):
         if not isinstance(other, Batch):
             return False
         return other.reference == self.reference
-    
+
     def __hash__(self):
         return hash(self.reference)
+
+    def __gt__(self, other):
+        if self.eta is None:
+            return False
+        if other.eta is None:
+            return True
+        return self.eta > other.eta
+
+
+def allocate(line: OrderLine, batches: list[Batch]) -> str:
+    batch = next(b for b in sorted(batches) if b.can_allocate(line))
+    batch.allocate(line)
+    return batch.reference
